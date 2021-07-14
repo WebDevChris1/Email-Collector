db4bbfc18120a8ab7f7724ccd400fd840f9b9a0c
danger2123 <sellmysolenyc@gmail.com>
2021-07-14 14:05:29 -0400

    finished JS

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let emailInput = document.getElementById("email-input");
 let subBtn = document.getElementById("sub-btn");
+let errorText = document.getElementById("error-text");
+let confirmText = document.getElementById("confirm-text");
 let emailList = [];
+let errorMsg = "";
 
 subBtn.addEventListener("click", function () {
-  emailList.push(emailInput.value);
-  console.log(emailList);
+  event.preventDefault();
+  if (emailInput.value === "") {
+    errorMsg = "Please enter your Email";
+    errorText.textContent = errorMsg;
+  } else {
+    emailInput.remove();
+    subBtn.remove();
+    errorText.textContent = "";
+    emailList.push(emailInput.value);
+    console.log(emailList);
+    confirmText.textContent = "Thank You!";
+  }
 });
